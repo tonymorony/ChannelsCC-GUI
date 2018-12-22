8cae9ae0f87d9e0835932ccb76680dda2e9935e3
Anton Lysakov <tlysakov@gmail.com>
2018-12-22 12:36:45 +0600

    made channels buttons more clever

--- gui/buttons/channelbutton.py
+++ gui/buttons/channelbutton.py
@@ -5,9 +5,16 @@ from lib import rpclib
 
 class ChannelListItemButton(ListItemButton):
 
-    def on_release(self):
+    def select(self):
         # setting active channel id after button release
+        self.background_color = [1., 0., 0., 1]
         application = App.get_running_app()
         application.active_channel_id = str(self.text)
         application.root.ids.userpage.ids.channelinformation.text = str(rpclib.channels_info(application.rpc_connection,
                                                                         application.active_channel_id))
+
+    def deselect(self):
+        application = App.get_running_app()
+        application.active_channel_id = ""
+        application.root.ids.userpage.ids.channelinformation.text = ""
+        self.background_color = [0., 1., 0., 1]

23072cb1b307997cc41cf6ca8ca5091273c8a236
Anton Lysakov <tlysakov@gmail.com>
2018-12-24 11:39:24 +0600

    little fix

--- gui/buttons/sendpaymentbutton.py
+++ gui/buttons/sendpaymentbutton.py
@@ -11,11 +11,15 @@ class SendPaymentButton(Button):
         application = App.get_running_app()
         channel = application.active_channel_id
         payment_sum = application.root.ids.paymentpage.ids.paymentamount.text
-        payment_hex = rpclib.channels_payment(application.rpc_connection, channel, str(int(Decimal(payment_sum))))
         try:
-            payment_txid = rpclib.sendrawtransaction(application.rpc_connection, payment_hex["hex"])
+            payment_hex = rpclib.channels_payment(application.rpc_connection, channel, str(int(Decimal(payment_sum))))
         except Exception as e:
-            application.root.ids.paymentpage.ids.paymentstatus.text = e
+            application.root.ids.paymentpage.ids.paymentstatus.text = str(e)
         else:
-            print(payment_txid)
-            application.root.ids.paymentpage.ids.paymentstatus.text = "Payment sent. TXID: " + str(payment_txid)
+            try:
+                payment_txid = rpclib.sendrawtransaction(application.rpc_connection, payment_hex["hex"])
+            except Exception as e:
+                application.root.ids.paymentpage.ids.paymentstatus.text = str(e)
+            else:
+                print(payment_txid)
+                application.root.ids.paymentpage.ids.paymentstatus.text = "Payment sent. TXID: " + str(payment_txid)
